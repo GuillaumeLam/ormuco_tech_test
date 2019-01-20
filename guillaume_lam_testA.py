a53448176b8ab3ea3cdd75805f3e2522e8af40a9
Guillaume Lam <guillaume.lam10100@gmail.com>
2019-01-20 02:32:56 -0500

    Added Range object, basic overlap method and simple main

---
+++ guillaume_lam_testA.py
@@ -0,0 +1,34 @@
+# Even though we have lists and tuples which are usable in python, having a
+# class will give more liberaty to expand and add more if need be later on
+class Range:
+    # left and right variables are needed initially
+    def __init__(self, left: int, right: int):
+        if left < right:
+            self.left = left
+            self.right = right
+        else:                                   # make sure the left bound is
+            self.left = right                   # smaller than the right
+            self.right = left
+        self.calc_len()
+
+    def calc_len(self):
+        self.len = self.right - self.left
+
+    # could possibly add methods for changing the left and right boundaries
+    # of the range rather than changing directly, then we can also recall
+    # the calc_len()
+
+# method to check if two Range objects overlap
+# as it takes as input the Range object, if lists/tuples were initially
+# available, porting them to Range objects would be simple
+def overlaps(rng1: Range, rng2: Range) -> bool:
+    return True
+
+def main():
+    rng1 = Range(5,1)
+    rng2 = Range(6,8)
+
+    print(overlaps(rng1, rng2))
+
+if __name__ == "__main__":
+    main()
